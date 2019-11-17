b2e194a93c43cba30dd28a1c81ae965e558c6294
jackcmac <jackcmac24@gmail.com>
2019-11-17 13:55:50 -0500

    starting layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,45 @@
 import { Component } from '@angular/core';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+
+interface CourseNode {
+  name: string;
+  children?: CourseNode[];
+}
+
+const TREE_DATA: CourseNode[] = [
+  {
+    name: 'Algorithms',
+    children: [
+      {name: 'Design and Analysis of Algorithms'},
+      {name: 'Algorithms and Data Structures'}
+    ]
+  },
+  {
+    name: 'Software Development',
+    children: [
+      {name: 'Human and Computer Interaction'},
+      {name: 'Software Engineering'},
+      {name: 'Software Development Foundations'},
+    ]
+  },
+  {
+    name: 'Topics',
+    children: [
+      {name: 'Artificial Intelligence'},
+      {name: 'Linear Algebra'},
+      {name: 'Network Programming'},
+      {name: 'Operating Systems'},
+      {name: 'Programming Systems'}
+    ]
+  }
+];
+
+interface FlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +48,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jack-mcelhinney';
+
+  private _transformer = (node: CourseNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  }
+
+  treeControl = new FlatTreeControl<FlatNode>(
+    node => node.level, node => node.expandable);
+
+  treeFlattener = new MatTreeFlattener(
+      this._transformer, node => node.level, node => node.expandable, node => node.children);
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FlatNode) => node.expandable;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTreeModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
